57e79fafd0284b52878e3024d5f9a337c07676b6
Jander Stival de Carvalho <janderscarvalho@gmail.com>
2023-09-18 13:55:21 -0300

    Projeto de estudos ReactRouts

---
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -0,0 +1,21 @@
+import styles from './Item.module.scss';
+import { Prato } from 'types/Prato';
+import TagsPrato from 'components/TagsPrato';
+
+export default function Item(props: Prato) {
+  const { title, description, photo } = props;
+  return (
+    <div className={styles.item}>
+      <div className={styles.item__imagem}>
+        <img src={photo} alt={title} />
+      </div>
+      <div className={styles.item__descricao}>
+        <div className={styles.item__titulo}>
+          <h2> {title} </h2>
+          <p> {description} </p>
+        </div>
+        <TagsPrato {...props} />
+      </div>
+    </div>
+  );
+}
